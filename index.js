4b8aa89ce7a04fd0e4d36d98620fad2efcd9dcd7
waluco <107259503+waluco@users.noreply.github.com>
2025-09-02 09:50:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-let vitoria = calculaPartidaRankeada(102, 1)
-let nivel = ["Ferro", "Bronze", "Prata", "Ouro", "Diamante", "Lendário", "Imortal"]
-
-if (vitoria < 10){
-    console.log("O Herói tem saldo de " + vitoria + " e está no nível " + nivel[0] )
-}else if (vitoria >= 11 && vitoria <= 20 ){
-    console.log("O Herói tem saldo de " + vitoria + " e está no nível " + nivel[1] )
-}else if (vitoria >= 21 && vitoria <= 50 ){
-    console.log("O Herói tem saldo de " + vitoria + " e está no nível " + nivel[2] )
-}else if (vitoria >= 51 && vitoria <= 80 ){
-    console.log("O Herói tem saldo de " + vitoria + " e está no nível " + nivel[3] )
-}else if (vitoria >= 81 && vitoria <= 90 ){
-    console.log("O Herói tem saldo de " + vitoria + " e está no nível " + nivel[4] )
-}else if (vitoria >= 91 && vitoria <= 100 ){
-    console.log("O Herói tem saldo de " + vitoria + " e está no nível " + nivel[5] )
-}else{
-    console.log("O Herói tem saldo de " + vitoria + " e está no nível " + nivel[6] )
+function calculaVitoria(vitorias, derrotas) {
+  saldo = vitorias - derrotas;
+  return saldo;
 }
 
-function calculaPartidaRankeada(vitoria, derrota) {
-    let resultado = vitoria - derrota
-    return(resultado)    
+function defineNivel(tipo) {
+  if (resultado <= 10) {
+    tipo = "Ferro";
+  } else if (resultado >= 11 && resultado <= 20) {
+    tipo = "Bronze";
+  } else if (resultado >= 21 && resultado <= 50) {
+    tipo = "Prata";
+  } else if (resultado >= 51 && resultado <= 80) {
+    tipo = "Ouro";
+  } else if (resultado >= 81 && resultado <= 90) {
+    tipo = "Diamante";
+  } else if (resultado >= 91 && resultado <= 100) {
+    tipo = "Lendário";
+  } else tipo = "Imortal";
+  return tipo
 }
+
+resultado = calculaVitoria(15, 1);
+tipo = defineNivel(resultado)
+//console.log(resultado);
+
+console.log("O Herói tem de saldo de: " + resultado + " está no nível de: " + tipo)
+
 
 
